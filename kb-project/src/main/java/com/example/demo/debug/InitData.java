df489623a354eeafbe7b45c7b50aa132f8c42e46
junwon <junwon1131@naver.com>
2025-06-25 13:34:52 +0900

    debug mysql

--- kb-project/src/main/java/com/example/demo/debug/InitData.java
+++ kb-project/src/main/java/com/example/demo/debug/InitData.java
@@ -4,18 +4,20 @@ import com.example.demo.entity.Role;
 import com.example.demo.repository.RoleRepository;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 @Component
 @RequiredArgsConstructor
-public class InitData {
+public class InitData implements ApplicationRunner {
 
     private final RoleRepository roleRepository;
 
-    @PostConstruct
+    @Override
     @Transactional
-    public void init() {
+    public void run(ApplicationArguments args) {
         if (roleRepository.count() == 0) {
             roleRepository.save(Role.builder().name("USER").build());
             roleRepository.save(Role.builder().name("ADMIN").build());
